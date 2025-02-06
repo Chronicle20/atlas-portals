f097522e213b550cf4fa91ce53a3ac5014296ae9
Chronicle20 <a.chronicle.20@gmail.com>
2025-02-06 08:38:15 -0500

    Remove unused code.

--- atlas.com/portals/main.go
+++ atlas.com/portals/main.go
@@ -11,26 +11,6 @@ import (
 const serviceName = "atlas-portals"
 const consumerGroupId = "Portal Service"
 
-type Server struct {
-	baseUrl string
-	prefix  string
-}
-
-func (s Server) GetBaseURL() string {
-	return s.baseUrl
-}
-
-func (s Server) GetPrefix() string {
-	return s.prefix
-}
-
-func GetServer() Server {
-	return Server{
-		baseUrl: "",
-		prefix:  "/api/pos/",
-	}
-}
-
 func main() {
 	l := logger.CreateLogger(serviceName)
 	l.Infoln("Starting main service.")
